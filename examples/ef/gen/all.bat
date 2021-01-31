78e162023130725221d18940c8eb4a60ad6ca8d1
Tomasz Strejczek <tomasz.strejczek@outlook.com>
2021-01-31 13:44:33 +0100

    Fixing regression in CI

--- examples/ef/gen/all.bat
+++ examples/ef/gen/all.bat
@@ -3,5 +3,5 @@ copy base.bzl nuget.bzl
 set tool=bazel run @io_bazel_rules_dotnet//tools/nuget2bazel:nuget2bazel.exe --
 #set tool=C:\rules_dotnet\tools\nuget2bazel\bin\Debug\netcoreapp3.1\nuget2bazel.exe
 
-%tool% add -p c:/rules_dotnet/docs/examples/ef/gen -b nuget.bzl -c nuget.json -i -l  microsoft.entityframeworkcore 3.1.3
+%tool% add -p %cd% -b nuget.bzl -c nuget.json -i -l  microsoft.entityframeworkcore 3.1.3
 
