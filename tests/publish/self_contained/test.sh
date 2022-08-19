97aea2d58ac33030f88d81abb0c15cea6ca93306
Daníel P. Purkhús <purkhusid@gmail.com>
2022-08-19 20:39:24 +0000

    WIP

--- tests/publish/self_contained/test.sh
+++ tests/publish/self_contained/test.sh
@@ -8,8 +8,8 @@ export JAVA_RUNFILES=""
 export RUNFILES_MANIFEST_FILE=""
 export RUNFILES_MANIFEST_ONLY=""
 
-# MacOS does not have readlink -f so we need to do some bash hacking
-realpath_macos() (
+# MacOS/Win do not have readlink -f so we need to do some bash hacking
+realpath_macos_win() (
   OURPWD=$PWD
   cd "$(dirname "$1")"
   LINK=$(readlink "$(basename "$1")")
@@ -29,10 +29,10 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     bin=$(readlink -f ./tests/publish/self_contained/self_contained/publish/linux-x64/app_to_publish)
     $bin
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    bin=$(realpath_macos ./tests/publish/self_contained/self_contained/publish/osx-x64/app_to_publish)
+    bin=$(realpath_macos_win ./tests/publish/self_contained/self_contained/publish/osx-x64/app_to_publish)
     $bin
 elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "win32" ]]; then
-    bin=$(readlink -f ./tests/publish/self_contained/self_contained/publish/win-x64/app_to_publish)
+    bin=$(realpath_macos_win ./tests/publish/self_contained/self_contained/publish/win-x64/app_to_publish)
     $bin
 else
     echo "Could not figure out which OS is running the test"
