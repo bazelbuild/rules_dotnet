cdeaf3ddc583f56c7f6fba617bdb64828ed684d9
Daniel P. Purkhús <purkhusid@gmail.com>
2022-08-31 13:57:41 +0000

    Jeez....

--- dotnet/private/tests/publish/framework_dependent/test.sh
+++ dotnet/private/tests/publish/framework_dependent/test.sh
@@ -22,7 +22,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     tar -xvf ./dotnet/private/tests/publish/framework_dependent/tar.tar
     ./app_to_publish
 elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "win32" ]]; then
-    tar -xvf ./dotnet/private/tests/publish/framework_dependent/tar.tar
+    unzip ./dotnet/private/tests/publish/framework_dependent/zip.zip ./
     ./app_to_publish.exe
 else
     echo "Could not figure out which OS is running the test"
--- dotnet/private/tests/publish/self_contained/test.sh
+++ dotnet/private/tests/publish/self_contained/test.sh
@@ -15,7 +15,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     tar -xvf ./dotnet/private/tests/publish/self_contained/tar.tar
     ./app_to_publish
 elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "win32" ]]; then
-    tar -xvf ./dotnet/private/tests/publish/self_contained/tar.tar
+    unzip ./dotnet/private/tests/publish/self_contained/zip.zip ./
     ./app_to_publish.exe
 else
     echo "Could not figure out which OS is running the test"
