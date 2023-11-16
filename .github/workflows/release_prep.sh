75a47763a8dce8f2c9ba76f0c68e1e1971c0369d
Daniel P. Purkhus <purkhusid@gmail.com>
2023-11-16 23:36:16 +0000

    WIP

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -5,28 +5,45 @@ set -o errexit -o nounset -o pipefail
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
-
 # The prefix is chosen to match what GitHub generates for source archives
+# This guarantees that users can easily switch from a released artifact to a source archive
+# with minimal differences in their code (e.g. strip_prefix remains the same)
 PREFIX="rules_dotnet-${TAG:1}"
 ARCHIVE="rules_dotnet-$TAG.tar.gz"
 
+# NB: configuration for 'git archive' is in /.gitattributes
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
+## Using Bzlmod with Bazel 7 (We require Bazel 7 and above)
+
+1. Add to your \`MODULE.bazel\` file:
+
+\`\`\`starlark
+bazel_dep(name = "rules_dotnet", version = "${TAG:1}")
+
+dotnet = use_extension("@rules_dotnet//dotnet:extensions.bzl", "dotnet")
+dotnet.toolchain(dotnet_version = "7.0.101")
+use_repo(dotnet, "dotnet_toolchains")
+
+register_toolchains("@dotnet_toolchains//:all")
+
+rules_dotnet_nuget_packages_extension = use_extension("@rules_dotnet//dotnet:paket.rules_dotnet_nuget_packages_extension.bzl", "rules_dotnet_nuget_packages_extension")
+use_repo(rules_dotnet_nuget_packages_extension, "paket.rules_dotnet_nuget_packages")
+\`\`\`
 
 ## Using WORKSPACE
 
-Paste this snippet into your \`WORKSPACE\` file:
+Paste this snippet into your `WORKSPACE.bazel` file:
 
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-
 http_archive(
     name = "rules_dotnet",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
-    url = "https://github.com/bazelbuild/rules_dotnet/releases/download/${TAG}/rules_dotnet-${TAG}.tar.gz",
+    url = "https://github.com/bazelbuild/rules_dotnet/releases/download/${TAG}/${ARCHIVE}",
 )
 
 load(
